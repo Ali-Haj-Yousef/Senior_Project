16c6163ab718aa8b8cc353c4037ec76f2a8df137
unknown <alihajyoussef00@gmail.com>
2025-04-07 12:14:28 +0300

    Plan&Scan

---
+++ Plan&Scan/Plan&Scan/Controllers/DataSheetsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Plan_Scan.Data;
+using Plan_Scan.Models;
+
+namespace Plan_Scan.Controllers
+{
+    public class DataSheetsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public DataSheetsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<StudentExamRegistration> registrations = _context.StudentExamRegistrations.ToList();
+            return View(registrations);
+        }
+    }
+}
---
+++ Plan&Scan/Plan&Scan/Migrations/20250406194325_CreateExamRegistrationTable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Plan_Scan.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateExamRegistrationTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentExamRegistrations",
+                columns: table => new
+                {
+                    RegId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Course = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Lang = table.Column<string>(type: "nvarchar(1)", nullable: false),
+                    Room = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    SeatNb = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    Time = table.Column<TimeOnly>(type: "time", nullable: false),
+                    ExamCode = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentExamRegistrations", x => x.RegId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentExamRegistrations");
+        }
+    }
+}
